86ba5f0a847bf54bba272357a27f724b45d5c8d4
lilydeller <lily@notunique.com>
2025-02-26 14:26:28 -0500

    output scaleasaurus

---
+++ src/main/java/com/Scaleasaurus/Scaleasaurus.java
@@ -0,0 +1,7 @@
+package model;
+
+public class Scaleasaurus {
+    public static void main(String[] args) {
+        System.out.println("Scale-A-Saurus");
+    }
+}
--- src/main/model /module-info.java
+++ src/main/model /module-info.java
@@ -1,7 +1,11 @@
 module com.scaleasaurus {
     requires javafx.controls;
     requires javafx.fxml;
+    requires json.simple;
 
     opens com.scaleasaurus to javafx.fxml;
     exports com.scaleasaurus;
+
+    opens model to javafx.fxml;
+    exports model;
 }
