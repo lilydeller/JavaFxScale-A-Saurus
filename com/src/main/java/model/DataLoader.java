25dfec9412981a3c02198961864f999f9ccd83d7
harrisonyim <hyim@email.sc.edu>
2025-03-03 14:20:13 -0500

    Added json simple

--- com/src/main/java/model/DataLoader.java
+++ com/src/main/java/model/DataLoader.java
@@ -1,5 +1,10 @@
 package model;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
 //import json simple
 
 public class DataLoader {
