0b15f2bd78b94c0450224e56f7772e9e3f861507
lilydeller <lily@notunique.com>
2025-03-03 20:57:54 -0500

    instrument/keys

--- com/src/main/java/model/MusicAppFacade.java
+++ com/src/main/java/model/MusicAppFacade.java
@@ -1,6 +1,3 @@
-/*
- * lily 
- */
 package model;
 
 public class MusicAppFacade {
--- com/src/main/java/model/MusicProgram.java
+++ com/src/main/java/model/MusicProgram.java
@@ -1,57 +1,63 @@
 package model;
 
+
 import music.Music;
 
+
 public class MusicProgram {
-    public void playSong() {
-        try {
-            playLine1();
-            Thread.sleep(300);
-            playLine2();
-            Thread.sleep(300);
-            playLine3();
-            Thread.sleep(300);
-            playLine1();
-            Thread.sleep(300);
-            playLine2();
-        } 
-        catch(Exception e){
-            System.out.println(e);
-        }
-    }
+   public void playSong() {
+       try {
+           playLine1();
+           Thread.sleep(300);
+           playLine2();
+           Thread.sleep(300);
+           playLine3();
+           Thread.sleep(300);
+           playLine1();
+           Thread.sleep(300);
+           playLine2();
+       }
+       catch(Exception e){
+           System.out.println(e);
+       }
+   }
 
-    private void playLine1() {
-        Music.playNote("C");
-        Music.playNote("C");
-        Music.playNote("G");
-        Music.playNote("G");
-        Music.playNote("A");
-        Music.playNote("A");
-        Music.playNote("G");
-    }
 
-    private void playLine2() {
-        Music.playNote("F");
-        Music.playNote("F");
-        Music.playNote("E");
-        Music.playNote("E");
-        Music.playNote("D");
-        Music.playNote("D");
-        Music.playNote("C");
-    }
+   private void playLine1() {
+       Music.playNote("C");
+       Music.playNote("C");
+       Music.playNote("G");
+       Music.playNote("G");
+       Music.playNote("A");
+       Music.playNote("A");
+       Music.playNote("G");
+   }
 
-    private void playLine3(){
-        Music.playNote("F");
-        Music.playNote("F");
-        Music.playNote("E");
-        Music.playNote("E");
-        Music.playNote("D");
-        Music.playNote("D");
-        Music.playNote("C");
-    }
 
-    public static void main(String[] args){
-        MusicProgram player = new MusicProgram();
-        player.playSong();       
-    }
+   private void playLine2() {
+       Music.playNote("F");
+       Music.playNote("F");
+       Music.playNote("E");
+       Music.playNote("E");
+       Music.playNote("D");
+       Music.playNote("D");
+       Music.playNote("C");
+   }
+
+
+   private void playLine3(){
+       Music.playNote("F");
+       Music.playNote("F");
+       Music.playNote("E");
+       Music.playNote("E");
+       Music.playNote("D");
+       Music.playNote("D");
+       Music.playNote("C");
+   }
+
+
+   public static void main(String[] args){
+       MusicProgram player = new MusicProgram();
+       player.playSong();      
+   }
 }
