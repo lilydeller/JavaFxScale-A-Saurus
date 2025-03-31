063b9b29ac42c3cc8474e4e00308c4d6e6a1d986
lilydeller <lily@notunique.com>
2025-03-31 13:27:20 -0400

    UsersTest

--- com/src/main/java/module-info.java
+++ com/src/main/java/module-info.java
@@ -3,7 +3,10 @@ module scaleasaurus {
     requires javafx.fxml;
     requires jfugue;
     requires json.simple;
+    requires junit;
+
 
     opens scaleasaurus to javafx.fxml;
     exports scaleasaurus;
+
 }
