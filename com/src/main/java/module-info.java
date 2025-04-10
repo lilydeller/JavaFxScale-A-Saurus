057469eae5754dc8f167276750033ceeb7fae313
ddobbinss <damiendobbins31@gmail.com>
2025-04-10 13:37:45 -0400

    cimmit to pull

--- com/src/main/java/module-info.java
+++ com/src/main/java/module-info.java
@@ -3,6 +3,7 @@ module scaleasaurus {
     requires javafx.fxml;
     requires jfugue;
     requires json.simple;
+    requires junit;
 
     opens scaleasaurus to javafx.fxml;
     exports scaleasaurus;
