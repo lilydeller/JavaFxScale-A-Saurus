21693aec8cf2b1362707aeae486fdec10570e7d7
ddobbinss <damiendobbins31@gmail.com>
2025-04-27 17:30:22 -0400

    styled song results to match rest of apps color pallete

--- com/src/main/java/controllers/SongResultsController.java
+++ com/src/main/java/controllers/SongResultsController.java
@@ -28,6 +28,7 @@ public class SongResultsController {
     @FXML
     public void initialize() {
 
+        resultsContainer.setStyle("-fx-background-color: #fdf5ec;");
         resultsContainer.getChildren().clear();
 
         if (filteredSongs == null || filteredSongs.isEmpty()) {
@@ -37,7 +38,7 @@ public class SongResultsController {
 
         for (Song song : filteredSongs) {
             VBox songBox = new VBox();
-            songBox.setStyle("-fx-background-color: #D8F9A0; -fx-border-color: #2F4F4F; -fx-padding: 10;");
+            songBox.setStyle("-fx-background-color: #D8F9A0;" + "-fx-border-color: #2F4F4F;" + "-fx-border-radius: 10;" + "-fx-background-radius: 10;" + "-fx-padding: 10;");
             songBox.setSpacing(5);
 
             Label name = new Label(song.getSongName() + " by " + song.getArtist());
@@ -48,6 +49,20 @@ public class SongResultsController {
             Button play = new Button("Play Along");
     
             Button save = new Button("Save to My Songs");
+
+            play.setStyle(
+        "-fx-background-color:rgb(23, 153, 58);" +
+        "-fx-background-radius: 8;" +
+        "-fx-border-radius: 8;" +
+        "-fx-text-fill: black;"
+    );
+
+    save.setStyle(
+        "-fx-background-color:rgb(23, 153, 58);" +
+        "-fx-background-radius: 8;" +
+        "-fx-border-radius: 8;" +
+        "-fx-text-fill: black;"
+    );
 
             play.setOnAction(e -> {
                 MusicAppFacade.getInstance().setCurrentSong(song);
